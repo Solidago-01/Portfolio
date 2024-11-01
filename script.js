e05e7e0360491837e75fa29fa5291535d6a23106
Autumn <l.a.gnadinger@gmail.com>
2024-11-01 07:12:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// const skew_card_a = document.getElementById("skew-card-a");
+// var breathe_direction = "up";
+// var breathe_amount = -30;
+
+// function breathe(){
+//     if (breathe_amount < -20 && breathe_direction == "up") {
+//         breathe_amount += .5;
+//         skew_card_a.style.transform = `rotate(${breathe_amount}deg)`;
+//         console.log(breathe_amount + breathe_direction);
+//     } else if (breathe_amount > -21 && breathe_direction == "up") {
+//         breathe_direction = "down";
+//         breathe_amount = -20;
+//         console.log(breathe_direction);
+//     } else if (breathe_amount > -30 && breathe_direction == "down") {
+//         breathe_amount -= .5;
+//         skew_card_a.style.transform = `rotate(${breathe_amount}deg)`;
+//         console.log(breathe_direction + breathe_amount);
+//     } else if (breathe_amount < -20) {
+//         breathe_direction = "up";
+//     }
+// }
+
+// setInterval(breathe, 100);
+// breathe();
+// var breathe_amount = 20;
+// skew_card_a.style.transform = `rotate(${breathe_amount}deg)`;
+ 
